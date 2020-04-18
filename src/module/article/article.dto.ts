f88a41e6e40971e7e9f8611375ca09d595d27aed
hotfireeagle <hotfireeagle@163.com>
2020-04-18 11:18:00 +0800

    fix: 修复对标签的校验

--- src/module/article/article.dto.ts
+++ src/module/article/article.dto.ts
@@ -9,6 +9,7 @@ export class CreateArticleDto {
   @IsNotEmpty({ message: '内容不能为空' })
   content: string // 文章内容
 
-  @IsNotEmpty({ message: '文章必须拥有标签' })
+  @IsNotEmpty({ message: '标签不能为空' })
+  @MinLength(1, { message: '文章必须拥有至少一个标签' })
   tags: Array<number> // 标签ID
 }
